ff691c0f7a0feae7bceb9128f697e16cc0ab74ed
Tracy Chen <ychen151@umd.edu>
2023-03-07 00:00:25 -0500

    upload files

---
+++ examples/handle.js
@@ -0,0 +1,41 @@
+/* eslint-disable react/prop-types */
+
+require('rc-slider/assets/index.less');
+require('rc-tooltip/assets/bootstrap.css');
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Tooltip = require('rc-tooltip');
+const Slider = require('rc-slider');
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+const Handle = Slider.Handle;
+
+const handle = (props) => {
+  const { value, dragging, index, ...restProps } = props;
+  return (
+    <Tooltip
+      overlay={value}
+      visible={dragging}
+      placement="top"
+      key={index}
+    >
+      <Handle {...restProps} />
+    </Tooltip>
+  );
+};
+
+const wrapperStyle = { width: 400, margin: 50 };
+ReactDOM.render(
+  <div>
+    <div style={wrapperStyle}>
+      <p>Slider with custom handle</p>
+      <Slider min={0} max={20} defaultValue={3} handle={handle} />
+    </div>
+    <div style={wrapperStyle}>
+      <p>Range with custom handle</p>
+      <Range min={0} max={20} defaultValue={[3, 10]} />
+    </div>
+  </div>,
+  document.getElementById('__react-content')
+);
---
+++ examples/range.js
@@ -0,0 +1,151 @@
+/* eslint react/no-multi-comp: 0 */
+require('rc-slider/assets/index.less');
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Slider = require('rc-slider');
+const Range = Slider.Range;
+
+const style = { width: 400, margin: 50 };
+
+function log(value) {
+  console.log(value); //eslint-disable-line
+}
+
+const CustomizedRange = React.createClass({
+  getInitialState() {
+    return {
+      lowerBound: 20,
+      upperBound: 40,
+      value: [20, 40],
+    };
+  },
+  onLowerBoundChange(e) {
+    this.setState({ lowerBound: +e.target.value });
+  },
+  onUpperBoundChange(e) {
+    this.setState({ upperBound: +e.target.value });
+  },
+  onSliderChange(value) {
+    log(value);
+    this.setState({
+      value,
+    });
+  },
+  handleApply() {
+    const { lowerBound, upperBound } = this.state;
+    this.setState({ value: [lowerBound, upperBound] });
+  },
+  render() {
+    return (
+      <div>
+        <label>LowerBound: </label>
+        <input type="number" value={this.state.lowerBound} onChange={this.onLowerBoundChange} />
+        <br />
+        <label>UpperBound: </label>
+        <input type="number" value={this.state.upperBound} onChange={this.onUpperBoundChange} />
+        <br />
+        <button onClick={this.handleApply}>Apply</button>
+        <br /><br />
+        <Range allowCross={false} value={this.state.value} onChange={this.onSliderChange} />
+      </div>
+    );
+  },
+});
+
+const DynamicBounds = React.createClass({
+  getInitialState() {
+    return {
+      min: 0,
+      max: 100,
+    };
+  },
+  onSliderChange(value) {
+    log(value);
+  },
+  onMinChange(e) {
+    this.setState({
+      min: +e.target.value || 0,
+    });
+  },
+  onMaxChange(e) {
+    this.setState({
+      max: +e.target.value || 100,
+    });
+  },
+  render() {
+    return (
+      <div>
+        <label>Min: </label>
+        <input type="number" value={this.state.min} onChange={this.onMinChange} />
+        <br />
+        <label>Max: </label>
+        <input type="number" value={this.state.max} onChange={this.onMaxChange} />
+        <br /><br />
+        <Range defaultValue={[20, 50]} min={this.state.min} max={this.state.max}
+          onChange={this.onSliderChange}
+        />
+      </div>
+    );
+  },
+});
+
+class ControlledRange extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: [20, 40, 60, 80],
+    };
+  }
+  handleChange = (value) => {
+    this.setState({
+      value,
+    });
+  }
+  render() {
+    return (
+      <Range value={this.state.value} onChange={this.handleChange}/>
+    );
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <div style={style}>
+      <p>Basic Range，`allowCross=false`</p>
+      <Range allowCross={false} defaultValue={[0, 20]} onChange={log} />
+    </div>
+    <div style={style}>
+      <p>Basic Range，`step=20` </p>
+      <Range step={20} defaultValue={[20, 20]} onBeforeChange={log} />
+    </div>
+    <div style={style}>
+      <p>Basic Range，`step=20, dots` </p>
+      <Range dots step={20} defaultValue={[20, 40]} onAfterChange={log} />
+    </div>
+    <div style={style}>
+      <p>Basic Range，disabled</p>
+      <Range allowCross={false} defaultValue={[0, 20]} onChange={log} disabled />
+    </div>
+    <div style={style}>
+      <p>Controlled Range</p>
+      <ControlledRange />
+    </div>
+    <div style={style}>
+      <p>Multi Range</p>
+      <Range count={3} defaultValue={[20, 40, 60, 80]} pushable />
+    </div>
+    <div style={style}>
+      <p>Multi Range, decimal step</p>
+      <Range count={3} defaultValue={[20, 40, 60, 80]} pushable step={0.1} />
+    </div>
+    <div style={style}>
+      <p>Customized Range</p>
+      <CustomizedRange />
+    </div>
+    <div style={style}>
+      <p>Range with dynamic `max` `min`</p>
+      <DynamicBounds />
+    </div>
+  </div>
+  , document.getElementById('__react-content'));
